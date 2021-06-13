ae9b97175ba9d3d37a1f4d0065fdc8ef2e7c01c6
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-06-13 18:10:48 -0500

    Update 1: check Betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,40 @@
 #include "sort.h"
+void swap_(int *xp, int *yp);
 /**
- * bubble_sort - Prints a list of integers
- * @array: ptr of array
- * @size: size of array
+ * swap_ - Prints a list of integers
+ * @xp: ptr of array
+ * @yp: size of array
  */
 
 void swap_(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-	
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
+/**
+ * selection_sort - Write a function that sorts an array of integers
+ * @array: ptr of array
+ * @size: size of array
+ */
 
 void selection_sort(int *array, size_t size)
 {
-    int min_num;
+	int min_num;
 	size_t j, i;
 
-    for (i = 0; i < size-1; i++)
-    {
+	for (i = 0; i < size - 1; i++)
+	{
 		min_num = i;
-        for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_num])
 				min_num = j;
 		}
 		if (array[min_num] < array[i])
 		{
-        	swap_(&array[min_num], &array[i]);
+			swap_(&array[min_num], &array[i]);
 			print_array(array, size);
 		}
-    }
+	}
 }
